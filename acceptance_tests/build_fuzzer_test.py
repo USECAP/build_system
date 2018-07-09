cf227c38ea541438ec6d8fe474943889f191420b
Code Intelligence <code@code-intelligence.com>
2018-07-09 13:35:16 +0200

    Project import generated by Copybara.
    GitOrigin-RevId: 92126250537ea1e66c093077aeb774d4d57aa172

--- acceptance_tests/build_fuzzer_test.py
+++
@@ -1,4 +0,0 @@
-import oss_fuzz.infra.helper as oss_fuzz
-
-if __name__ == '__main__':
-    oss_fuzz.main(["-h"])
--- deb_deps/fetch_package_contents.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-for outfile in "$@"; do
-    distro=$(basename $outfile)
-    curl http://archive.ubuntu.com/ubuntu/dists/${distro}/Contents-amd64.gz | gunzip > "$outfile"
-done
-
